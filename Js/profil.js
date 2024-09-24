d6fe67fdaa259bd9c6fa8682b71622bec3989991
Vikas Basavi <156122503+Vikasvs91@users.noreply.github.com>
2024-09-24 10:41:22 +0530

    Your commit message here

--- Js/profil.js
+++ Js/profil.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+   
+    //Cursor movement
     const cursor=document.querySelector('.cursor');
+    
             document.addEventListener('mousemove',(vikas)=>{
                 cursor.style.left=vikas.pageX+'px';
                 cursor.style.top=vikas.pageY+'px';
