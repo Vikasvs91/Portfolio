c656428d1261814067b4f83ec7fe973bdd4751ae
Vikas Basavi <156122503+Vikasvs91@users.noreply.github.com>
2024-09-24 11:24:56 +0530

    Delete profil.js

--- profil.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cursor=document.querySelector('.cursor');
-            document.addEventListener('mousemove',(vikas)=>{
-                cursor.style.left=vikas.pageX+'px';
-                cursor.style.top=vikas.pageY+'px';
-
-            });
-});
